324aa181839ee815f01f3fa31fd31e68e7e6e7bc
GustavBSS <109105795+GustavBSS@users.noreply.github.com>
2024-04-07 16:05:38 -0300

    Implementação: do módulo 'allauth', integração com o GitHub OAuth (possibilidade de login com a conta do GitHub - incompleto) e da página de administração

--- app/settings.py
+++ app/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.github',
     'ecommerce',
 ]
 
@@ -47,8 +52,29 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+
+ACCOUNT_EMAIL_VERIFICATIONS = 'none'
+
+LOGIN_REDIRECT_URL = '/'
+
+SOCIALACCOUNT_PROVIDERS = {
+    'github': {
+        'APP': {
+            'client_id': '123',
+            'secret': '456',
+        }
+    }
+}
 
 ROOT_URLCONF = 'app.urls'
 
